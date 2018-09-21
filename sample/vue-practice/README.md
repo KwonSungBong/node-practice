935f92cc05d64a067eac9d416fc63fcb8e184648
권성봉 <sbkwon@suholdings.com>
2018-09-21 11:09:00 +0900

    test

---
+++ sample/vue-practice/README.md
@@ -0,0 +1,9 @@
+# vue-practice
+
+
+https://github.com/koajs/examples/tree/master/templates
+
+
+https://github.com/alexmingoia/koa-router
+
+
---
+++ sample/vue-practice/app.js
@@ -0,0 +1,17 @@
+import Koa from 'koa'
+import path from 'path'
+import views from 'koa-views'
+import router from './routes'
+
+const app = new Koa()
+
+app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }))
+
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(3000, () => console.log('server started 3000'))
+
+export default app
+
