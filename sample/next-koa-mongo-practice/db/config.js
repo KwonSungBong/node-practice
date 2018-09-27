0e98bc52cac0f687546c5e27b0caf51d25c53ecd
권성봉 <sbkwon@suholdings.com>
2018-09-27 10:37:15 +0900

    test

---
+++ sample/next-koa-mongo-practice/db/config.js
@@ -0,0 +1,9 @@
+const development = 'mongodb://192.168.99.100/mydb';
+const production = 'mongodb://192.168.99.100/mydb';
+const test = 'mongodb://192.168.99.100/mydb-test';
+
+module.exports = {
+    development,
+    production,
+    test
+}
